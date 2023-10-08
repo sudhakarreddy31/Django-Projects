0f0a0d7d84c76b31deba35c14b763884b4798e97
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-08 12:31:38 +0530

    Using APIViews in DRF

---
+++ project30/webapp/views.py
@@ -0,0 +1,43 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from webapp.models import Employee
+from webapp.serializers import  EmployeeSerializer
+from django.shortcuts import get_object_or_404
+
+
+
+class EmployeeListView(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+            serializer = EmployeeSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EmployeeDetailView(APIView):
+    def get(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        employee.delete()
+        return JsonResponse({"message": "Employee deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
